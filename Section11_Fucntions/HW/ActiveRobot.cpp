b8edfc9ba9c15ce936ebe6839728c50c5025c8a0
Ali El-bana <alitheboss922@gmail.com>
2023-07-10 20:08:46 +0300

    Adding a solution of Max of 6 numbers problem

---
+++ Section11_Fucntions/HW/ActiveRobot.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int main(void) 
+{
+
+	system( "cls" ) ;
+	
+	int rows 						;
+	int columns 					;
+	unsigned int CommandsNum 		;
+	unsigned int direction 			;
+	unsigned int steps 				;
+	int currentRow{0}		;
+	int currentColumn{0} 	;
+	
+	cout<<endl <<"Enter the number of rows: " ;
+	cin>>rows ;
+	
+	cout<<endl <<"Enter the number of columns: " ;
+	cin>>columns ;
+	
+	int array[rows][columns] ;
+	
+	cout<<endl <<"Enter the number of commands: " ;
+	cin>>CommandsNum ;
+	
+	for( int k = 0; k < CommandsNum; k++ )
+	{
+		
+		cout<<endl <<"Enter the direction: " ;
+		cin>>direction ;
+		
+		cout<<endl <<"Enter the steps: " ;
+		cin>>steps ;
+		
+		switch( direction )
+		{
+			// UP //
+			case 1:     
+				
+				currentRow = currentRow - steps ;
+				
+				if( currentRow < 0 )
+				{
+					currentRow = -( currentRow % (-rows) ) ;
+					
+					currentRow = rows - currentRow ;
+					
+					if( currentRow == rows )
+					{
+						currentRow = 0 ;
+					}
+				}
+				
+				
+			break ;
+			
+			//---------------------------------------------------//
+			
+			// RIGHT //
+			case 2:     
+				
+				currentColumn = (currentColumn + steps) % columns ;
+				
+			break ;
+			
+			//---------------------------------------------------//
+			
+			// DOWN //
+			case 3:     
+				
+				currentRow = (currentRow + steps) % rows ;
+				
+			break ;
+			
+			//---------------------------------------------------//
+			
+			// LEFT //
+			case 4:     
+				
+				currentColumn = currentColumn - steps ;
+				
+				if( currentColumn < 0 )
+				{
+					currentColumn = -( currentColumn % (-columns) ) ;
+					
+					currentColumn = columns - currentColumn ;
+					
+					if( currentColumn == columns )
+					{
+						currentColumn = 0 ;
+					}
+				}
+				
+			break ;
+			
+			//---------------------------------------------------//
+			
+			// DEFAULT //
+			default:     
+				
+				// Do nothing //
+				
+			break ;
+			
+		}
+		
+		cout<<endl <<"Current position: " <<'(' <<currentRow <<',' <<' ' <<currentColumn <<')' <<endl ;
+		
+	}
+	
+	
+	return 0 ;
+	
+}
---
+++ Section11_Fucntions/HW/MaxOf6.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int max(int a, int b, int c) ;
+int max(int a, int b, int c, int d) ;
+int max(int a, int b, int c, int d, int e) ;
+int max(int a, int b, int c, int d, int e, int f) ;
+
+int main(void) 
+{
+
+	system( "cls" ) ;
+	
+	int a, b, c, d, e, f = 0 ;
+
+	int maxNum = 0 ;
+
+	cout<<endl <<"Enter the six numbers: " ;
+	cin>>a >>b >>c >>d >>e >>f ;
+
+	maxNum = max( a, b, c, d, e, f ) ;
+	
+	cout<<endl <<"The maximum number of the six is: " <<maxNum ;
+
+	return 0 ;
+	
+}
+
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+
+int max(int a, int b, int c) 
+{
+	
+	int max = a ;
+	
+	if( max < b  )
+	{
+		max = b ;
+	}
+	else
+	{
+		max = c ;
+	}
+	
+	return max ;
+	
+}
+
+////////////////////////////////////////////////////////////////////
+
+int max(int a, int b, int c, int d)
+{
+	
+	int result = 0 ;
+	
+	int max1 = d ;
+	
+	int max2 = max( a,  b,  c ) ;
+	
+	if( max1 > max2 )
+	{
+		result = max1 ;
+	}
+	else
+	{
+		result = max2 ;
+	}
+	
+	return result ;
+	
+}
+
+////////////////////////////////////////////////////////////////////
+
+int max(int a, int b, int c, int d, int e)
+{
+	
+	int result = 0 ;
+	
+	int max1 = e ;
+	
+	int max2 = max( a,  b,  c, d ) ;
+	
+	if( max1 > max2 )
+	{
+		result = max1 ;
+	}
+	else
+	{
+		result = max2 ;
+	}
+	
+	return result ;
+	
+}
+
+////////////////////////////////////////////////////////////////////
+
+int max(int a, int b, int c, int d, int e, int f)
+{
+	
+	int result = 0 ;
+	
+	int max1 = f ;
+	
+	int max2 = max( a,  b,  c, d, e ) ;
+	
+	if( max1 > max2 )
+	{
+		result = max1 ;
+	}
+	else
+	{
+		result = max2 ;
+	}
+	
+	return result ;
+	
+}
+
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
