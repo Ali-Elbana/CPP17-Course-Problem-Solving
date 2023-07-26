64f99d8f2d042634e6383db7d9495a672ff3fa4d
Ali El-bana <alitheboss922@gmail.com>
2023-07-26 18:30:17 +0300

    library project files

---
+++ Section15_Project#2(library)/main.cpp
@@ -0,0 +1,22 @@
+/* FILENAME: main.cpp 
+*  Author:  Ali El-bana
+*  Version:  V1.0
+*  DATE:   Fri 07/21/2023
+*/
+
+#include <iostream>
+#include "library_interface.hpp"
+
+using namespace std ;
+
+int main( void )
+{
+	
+	system( "cls" ) ;
+	
+
+	
+}//{main bracket}//
+
+
+
